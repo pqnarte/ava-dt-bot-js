0c4d8f6eca2033200e74e5e6e56fb750736f32fc
pqnarte <37677471+pqnarte@users.noreply.github.com>
2018-09-24 20:20:14 +0100

    Update weapons.js

--- commands/weapons.js
+++ commands/weapons.js
@@ -27,7 +27,7 @@ module.exports.run = async (client, message, args) => {
           var j = 0;
           var content = $('.mw-content-ltr').find('li').each(function(i,elem) {
             //text += $(this).text()+'\n'
-            text += '[$(this).text()+']('+https://ava-dog-tag.wikia.com'+$(this).children().attr('href')+')\n'
+            text += '['+$(this).text()+'](https://ava-dog-tag.wikia.com'+$(this).children().attr('href')+')\n'
             text2 += 'https://ava-dog-tag.wikia.com'+$(this).children().attr('href')+'\n';
           });
           data[i] = text;
