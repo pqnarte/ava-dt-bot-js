36af1d1ffdca9e97a7f211fa2d1d4fbfeb7f7dbf
pqnarte <37677471+pqnarte@users.noreply.github.com>
2018-11-16 22:27:49 +0000

    Update maps.js

--- commands/maps.js
+++ commands/maps.js
@@ -25,9 +25,9 @@ module.exports.run = async (client, message, args) => {
           text = "";
           text2 = "";
           var j = 0;
-          var content = $('.mw-content-ltr').find('li').each(function(i,elem) {
-            text += '['+$(this).text()+'](https://ava-dog-tag.wikia.com'+$(this).children().attr('href')+')\n'
-            text2 += 'https://ava-dog-tag.wikia.com'+$(this).children().attr('href')+'\n';
+          var content = $('.category-page__members').find('a').each(function(i,elem) {
+            text += '['+$(this).text()+'](https://ava-dog-tag.wikia.com'+$(this).attr('href')+')\n'
+            text2 += 'https://ava-dog-tag.wikia.com'+$(this).attr('href')+'\n';
           });
           data[i] = text;
           links[i] = text2;
